e285d4281c395f2040dfde8701bcd4ff68a0a4cf
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-07-09 09:31:59 +0700

    express server generator

--- node-express/index.js
+++ node-express/index.js
@@ -5,6 +5,8 @@ const hostname = 'localhost';
 const port = 3000;
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
 
 const app = express();
 
@@ -12,24 +14,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send detail of the dishes' + req.params.dishId + 'to you');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes/' + req.params.dishId);
-})
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-})
-
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
