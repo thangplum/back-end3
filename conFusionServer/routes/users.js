f9c5bb244d92feb74939a84fdc0327b43a5bea96
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-07-12 11:15:35 +0700

    Using JWT to authenticate with passport

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -5,6 +5,7 @@ var User = require('../models/user');
 var router = express.Router();
 router.use(bodyParser.json());
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -30,9 +31,10 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res) => {
