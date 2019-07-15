5add7b270092443415b82d1c578414db88578bde
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-07-15 15:11:37 +0700

    Integrating Client and Server

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -10,7 +10,7 @@ const Leaders = require('../models/leaders.js');
 leaderRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, (req, res, next) => {
-	Leaders.find({})
+	Leaders.find(req.query)
 	.then((leader) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', "application/json");
